289202203bf019a5da423eb314b46402ef4fbacd
Taner Özer <tanerozer16@gmail.com>
2022-07-17 17:51:54 +0300

    app.js updated

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ const App =() => {
 
     const [tvSeries, setTvSeries] = useState(null)
     const fetchData = async () =>{
-
         const response = await fetch('/.netlify/functions/setTvSeries')
         const responseBody = await response.json()
         setTvSeries(responseBody.data.tvseries_data.values)
-      }
+    }
     
-      useEffect(() => {
+    useEffect(() => {
         fetchData()
-      }, [])
+    }, [])
     
-      console.log(tvSeries)
+    console.log(tvSeries)
 
+    return (
+        <div className="App">
+            <h1>My Series</h1>
+                {tvSeries?.map(tvSeries => (
+                <div className="series">
+                    <h2>{tvSeries.name}</h2>
+                    <p>{tvSeries.rating}</p>
+                </div>
+        ))}
+        </div>
+    );
 }
-
 export default App;

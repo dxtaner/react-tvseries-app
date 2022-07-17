dcaa0a0821127a435c70bd44b9d6306e5964cb9c
Taner Özer <tanerozer16@gmail.com>
2022-07-17 17:53:55 +0300

    end com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react'
 
-const App =() => {
+const App = () => {
 
     const [tvSeries, setTvSeries] = useState(null)
     const fetchData = async () =>{

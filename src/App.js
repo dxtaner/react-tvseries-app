f0d3f1b85bdc3fce1873a2b0b329963fa4698c04
Taner Özer <tanerozer16@gmail.com>
2022-07-17 17:49:44 +0300

    app js edit"ed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { useState, useEffect } from 'react'
+
 const App =() => {
 
-  const [tvSeries, setTvSeries] = useState(null)
+    const [tvSeries, setTvSeries] = useState(null)
+    const fetchData = async () =>{
 
-  const fetchData = async () =>{
+        const response = await fetch('/.netlify/functions/setTvSeries')
+        const responseBody = await response.json()
+        setTvSeries(responseBody.data.tvseries_data.values)
+      }
+    
+      useEffect(() => {
+        fetchData()
+      }, [])
+    
+      console.log(tvSeries)
 
-    const response = await fetch('/.netlify/functions/setTvSeries')
-    const responseBody = await response.json()
-    setTvSeries(responseBody.data.hotel_data.values)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  console.log(tvSeries)
-
-  return (
-    <div className="App">
-     
-    </div>
-  );
 }
 
 export default App;

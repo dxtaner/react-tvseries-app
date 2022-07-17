62f9b1c1149bd36eb24c83f4072d780a96f6b8a6
Taner Özer <tanerozer16@gmail.com>
2022-07-17 17:39:33 +0300

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+const App =() => {
 
-function App() {
+  const [tvSeries, setTvSeries] = useState(null)
+
+  const fetchData = async () =>{
+
+    const response = await fetch('/.netlify/functions/setTvSeries')
+    const responseBody = await response.json()
+    setTvSeries(responseBody.data.hotel_data.values)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(tvSeries)
+
   return (
     <div className="App">
      
